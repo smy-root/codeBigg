3f39cc83ad70170b1184343851be4a3a74be9828
shimengyuan <2399437876@qq.com>
2019-07-23 23:23:48 +0800

    new

--- 1.js
+++ 1.js
@@ -8,7 +8,6 @@ app.use(bodyParser.json())
 // 处理字符串
 app.use(bodyParser.urlencoded({extends:false}))
 var formPath=path.resolve(__dirname)
-console.log(formPath)
 app.use(express.static(formPath))
 app.get('/',(req,res)=>{
     res.send('首页')
